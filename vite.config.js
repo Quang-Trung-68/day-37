ba3c8a06aef94d2d51085af12860ca6b53138950
Quang-Trung-68 <trungdang.dqt@gmail.com>
2025-11-08 22:36:20 +0700

    fix: update package.json and vite.config.js to include vite-plugin-spa-fallback

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import spaFallbackPlugin from "vite-plugin-spa-fallback";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/day-37/",
-  plugins: [react()],
+  plugins: [react(), spaFallbackPlugin()],
   resolve: {
     alias: {
       "@": "/src",
