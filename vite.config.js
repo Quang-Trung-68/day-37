f38bd734d81ad650f377ce54a2984202d6d3ba1f
Quang-Trung-68 <trungdang.dqt@gmail.com>
2025-11-08 22:39:20 +0700

    fix: remove homepage, predeploy, and deploy scripts from package.json; update vite.config.js to exclude spa-fallback plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import spaFallbackPlugin from "vite-plugin-spa-fallback";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/day-37/",
-  plugins: [react(), spaFallbackPlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": "/src",
